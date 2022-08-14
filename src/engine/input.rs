5132263f9c2028f16a72bd29ea28366678cdcac7
Jacopo Libè <jacopo.libe@gmail.com>
2022-08-14 20:26:09 +0200

    Start work on inputs

--- src/engine/input.rs
+++ src/engine/input.rs
@@ -1,7 +1,26 @@
 use std::error::Error;
 
 use mint::Vector3;
-use openxr::{FrameState, Space, Time};
+use openxr::{FrameState, Space, Time, ActionSet, Instance};
+
+pub struct InputContext {
+    pub action_sets: Vec<ActionSet>,
+}
+
+impl InputContext {
+    pub fn init(xr_instance: &Instance) -> InputContext {
+
+        let default_set = xr_instance
+            .create_action_set("default", "Default controller actions", 0)
+            .unwrap();
+
+        //default_set.
+
+        InputContext {
+            action_sets: vec!(default_set),
+        }
+    }
+}
 
 pub fn get_view_acceleration_vector(reference_space: &Space, view_space: &Space, frame_state: &FrameState) -> Result<Vector3<f32>, Box<dyn Error>> {
     let origin_time = Time::from_nanos(frame_state.predicted_display_time.as_nanos() - frame_state.predicted_display_period.as_nanos());
